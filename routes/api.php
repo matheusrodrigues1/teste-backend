d51015d462ade66bb28088764131ba9d668bd670
matheusrodrigues1 <matech00@outlook.com>
2023-08-04 18:01:58 -0300

    criação da api

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrestadorController;
+use App\Http\Controllers\ServicoController;
+use App\Http\Controllers\PrestadorServicoController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/prestador_servico', [PrestadorServicoController::class, 'store']);
+Route::apiResource('prestadores', PrestadorController::class);
+Route::apiResource('servicos', ServicoController::class);
